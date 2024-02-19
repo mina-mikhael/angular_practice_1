8d74dcaa6818ba5967c8a2e1c9315040e87dc061
Mina-Mikhael <mina.s.mikhael@gmail.com>
2024-02-18 23:59:23 -0500

    added some alerts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
-import { ServersComponent } from './components/servers/servers.component';
-import { ServerComponent } from './components/server/server.component';
+import { WarningAlertComponent } from './components/alerts/warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './components/alerts/success-alert/success-alert.component';
 
 @NgModule({
-  declarations: [AppComponent, ServersComponent, ServerComponent],
+  declarations: [AppComponent, WarningAlertComponent, SuccessAlertComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
